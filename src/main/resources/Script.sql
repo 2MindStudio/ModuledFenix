e7c0f013bc44ec8d64818481c959b21c5d10c16b
David <david26022005@gmail.com>
2026-03-11 11:09:31 +0100

    Data Base modified. I hope this is the final modification.

--- src/main/resources/Script.sql
+++ src/main/resources/Script.sql
@@ -13,11 +13,11 @@ create table wallet
 create table client(
 	id Int auto_increment PRIMARY KEY,
 	username VARCHAR(20) NOT NULL,
-	password_Hash VARCHAR(20) NOT NULL,
-	walletId INT NOT NULL,
+	password_hash VARCHAR(20) NOT NULL,
+	wallet_id INT NOT NULL,
 
 	CONSTRAINT fk_wallet_client
-	FOREIGN KEY (walletId)
+	FOREIGN KEY (wallet_id)
 	REFERENCES wallet (id)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE
@@ -26,8 +26,8 @@ create table client(
 create table player
 (
 	id Int primary key,
-	otherAlias VARCHAR(20),
-	appsPlayed Int not null,
+	other_alias VARCHAR(20),
+	apps_played Int not null,
 
 	CONSTRAINT fk_player_client
 	FOREIGN KEY (id)
@@ -39,8 +39,8 @@ create table player
 create table developer
 (
 	id Int primary key,
-	brandName VARCHAR(20) not null,
-	appsPublished Int not null,
+	brand_name VARCHAR(20) not null,
+	apps_published Int not null,
 
 	CONSTRAINT fk_developer_client
 	FOREIGN KEY (id)
@@ -53,24 +53,24 @@ create table creditCard
 (
 	number VARCHAR(16) primary key,
 	cvc Int not null,
-	expiredDate Date not null
+	expired_date Date not null
 );
 
 create table contain
 (
-	walletId Int not null,
-	cardNumber VARCHAR(16) not null,
+	wallet_id Int not null,
+	card_number VARCHAR(16) not null,
 
-	primary key(walletId, cardNumber),
+	primary key(wallet_id, card_number),
 
 	CONSTRAINT fk_contain_wallet
-	FOREIGN KEY (walletId)
+	FOREIGN KEY (wallet_id)
 	REFERENCES wallet (id)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE,
 
 	CONSTRAINT fk_contain_creditCard
-	FOREIGN KEY (cardNumber)
+	FOREIGN KEY (card_number)
 	REFERENCES creditCard (number)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE
@@ -78,70 +78,70 @@ create table contain
 
 create table project(
 	id Int auto_increment primary key,
-	devId Int,
+	dev_id Int,
 	title VARCHAR(20) not null,
 	description VARCHAR(100),
 	portray longblob,
 
 	CONSTRAINT fk_project_developer
-	FOREIGN KEY (devId)
+	FOREIGN KEY (dev_id)
 	REFERENCES developer (id)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE
 );
 
 create table genre(
-	idGenero Int auto_increment primary key,
+	id_genero Int auto_increment primary key,
 	name VARCHAR(30) not null
 );
 
 create table projectGenre(
-	devId Int not null,
-	idGenero Int not null,
+	dev_id Int not null,
+	id_genero Int not null,
 
-	primary key(devId, idGenero),
+	primary key(dev_id, id_genero),
 
 	CONSTRAINT fk_projectGenre_project
-	FOREIGN KEY (devId)
-	REFERENCES project(devId)
+	FOREIGN KEY (dev_id)
+	REFERENCES project(dev_id)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE,
 
 	CONSTRAINT fk_projectGenre_genre
-	FOREIGN KEY (idGenero)
-	REFERENCES genre(idGenero)
+	FOREIGN KEY (id_genero)
+	REFERENCES genre(id_genero)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE
 );
 
 create table application(
-	devId Int primary key,
-	launchedDate DATE,
-	receiptId INT,
+	dev_id Int primary key,
+	launched_date DATE,
+	receipt_id INT,
 
 	CONSTRAINT fk_application_project
-	FOREIGN KEY (devId)
-	REFERENCES project(devId)
+	FOREIGN KEY (dev_id)
+	REFERENCES project(dev_id)
 	ON DELETE restrict
 	ON UPDATE cascade
 );
 
 create table run(
-	clientId Int not null,
-	devId Int not null,
-	counterHours Int,
+	client_id Int not null,
+	dev_id Int not null,
+	counter_hours Int,
 
-	primary key(clientId, devId),
+	primary key(client_id, dev_id),
 
 	CONSTRAINT fk_run_client
-	FOREIGN KEY (clientId)
+	FOREIGN KEY (client_id)
 	REFERENCES client(id)
 	ON DELETE restrict
 	ON UPDATE cascade,
 
 	CONSTRAINT fk_run_application
-	FOREIGN KEY (devId)
-	REFERENCES application(devId)
+	FOREIGN KEY (dev_id)
+	REFERENCES application(dev_id)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE
 );
@@ -149,37 +149,37 @@ create table run(
 create table receipt(
 	id Int auto_increment primary key,
 	price DECIMAL(10,2) not null,
-	devId Int not null,
+	dev_id Int not null,
 
 	CONSTRAINT fk_receipt_application
-	FOREIGN KEY (devId)
-	REFERENCES application(devId)
+	FOREIGN KEY (dev_id)
+	REFERENCES application(dev_id)
 	ON DELETE restrict
 	ON UPDATE cascade
 );
 
 create table pay(
-	receiptId Int not null,
-	walletId Int not null,
-	clientId Int not null,
-	payDate DATE not null,
+	receipt_id Int not null,
+	wallet_id Int not null,
+	client_id Int not null,
+	pay_date DATE not null,
 
-	primary key(receiptId, walletId),
+	primary key(receipt_id, wallet_id),
 
 	CONSTRAINT fk_pay_receipt
-	FOREIGN KEY (receiptId)
+	FOREIGN KEY (receipt_id)
 	REFERENCES receipt(id)
 	ON DELETE restrict
 	ON UPDATE cascade,
 
 	CONSTRAINT fk_pay_wallet
-	FOREIGN KEY (walletId)
+	FOREIGN KEY (wallet_id)
 	REFERENCES wallet (id)
 	ON DELETE RESTRICT
 	ON UPDATE cascade,
 
 	CONSTRAINT fk_pay_client
-	FOREIGN KEY (clientId)
+	FOREIGN KEY (client_id)
 	REFERENCES client(id)
 	ON DELETE RESTRICT
 	ON UPDATE cascade
